4ab5b8782b14f9b8a7fab822549be8d174bea691
EduardMLT <eddku@i.ua>
2023-08-11 09:04:57 +0300

    initial

--- src/components/Transactions/TransactionsData.js
+++ src/components/Transactions/TransactionsData.js
@@ -1,4 +1,31 @@
+
+import TableRowData from "./TransactionsTable";
+import {TransactionTable, TableHeader, TableRow} from './TransactionsData.styles'
+
+
 export const TransactionsData = ({ transact }) => {
     console.log(transact)
-    return 
+    return (
+        <TransactionTable className="transaction-history">
+            <TableHeader className="transaction-header">
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </TableHeader>
+
+            <tbody>
+                {transact.map((transactionsItem, index) => {
+                    const isEvenRow = index % 2 === 0;
+                    return (
+                        <TableRow key={transactionsItem.id} className="transaction-row" $iseven={isEvenRow}>
+                            <TableRowData items={transactionsItem} />
+                        </TableRow>
+                    );
+                })}
+			
+            </tbody>
+        </TransactionTable>
+    );
 }
